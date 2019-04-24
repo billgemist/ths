fae40f64bd08c62a8df0e589ea6ff4ff0ce65002
billgemist <billgemist@gmail.com>
2019-04-24 10:37:17 +0200

    updated instructions and ub44.c

--- ubnum/ub_44/ub44.c
+++ ubnum/ub_44/ub44.c
@@ -1,9 +1,24 @@
-//undefined behavior number tade
+//undefined behavior number 44
 #include <stdio.h>
 
 int main(){
 	
+	
+	char ub[18] = "UndefinedBehavior";
+	
+	
+	char *ptr = ub + 18;	// pointing to just beyond
 
+	char var = *ptr; //undefined behavior number 44
+
+	char aaa = 'F';
+	/*
+	*** Disclosure: The printf's will be removed in the test it is just for mine convinience!
+	*/
+
+
+	printf("var = %c\n", var);
+	printf("AAA = %c\n", aaa);
     
 	return 0;
 }

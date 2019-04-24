e5ba1f85b06e32bcb8463d6c64300e1c9f81019f
billgemist <billgemist@gmail.com>
2019-04-24 14:32:44 +0200

    ub2 added with test ready

--- ubnum/ub_94/ub94.c
+++ ubnum/ub_94/ub94.c
@@ -1,9 +1,9 @@
 //undefined behavior number 94
 #include <stdio.h>
-
 int main(){
 	
 	/* test pending see: https://riptutorial.com/c/example/2150/copying-overlapping-memory */
-    
+	
 	return 0;
-}
+	
+	}

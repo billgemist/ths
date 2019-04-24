f4e64a150b9b2ce7c6ac9ccc8d599869d15fae14
billgemist <billgemist@gmail.com>
2019-04-24 16:54:43 +0200

    ub5

---
+++ ubnum/ub_5/ub5.c
@@ -0,0 +1,17 @@
+//undefined behavior number 5
+#include <stdio.h>
+
+int main(){
+	
+	int val = 10;
+
+	int var = $3; //undefined behavior 
+ 
+	//int sum = val + var;
+
+	//printf("val = %d\n", val);
+	//printf("var = %d\n", var);
+	//printf("sum = %d\n", sum);
+    
+	return 0;
+}

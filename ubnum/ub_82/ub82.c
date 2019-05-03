a871d91cf7f5164e840d56563ad0f7525ab0b863
billgemist <billgemist@gmail.com>
2019-05-03 11:08:59 +0200

    82

---
+++ ubnum/ub_82/ub82.c
@@ -0,0 +1,15 @@
+//undefined behavior number tade
+#include <stdio.h>
+
+int func(int a, int b){
+	//return (a+b);
+}
+
+int main(){
+	
+	int val = func(1, 2);
+    
+	printf("%d\n", val);
+
+	return 0;
+}

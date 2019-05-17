dc6393ef926806031e59491d2c42a613de75b9d0
billgemist <billgemist@gmail.com>
2019-05-17 12:22:29 +0200

    results ub61

--- ubnum/ub_61/ub61_2.c
+++ ubnum/ub_61/ub61_2.c
@@ -1,28 +1,20 @@
 //undefined behavior number 61
-/*in this example the value of "val" changed by pointer f through pointer mptr(which is const).
-*ub61_2.c will change the value of "val" via pointer without const directly
-*/
+/*in this example value of "val" will change through pointer
+*without const type qualifier*/
 #include <stdio.h>
 
-void func(const int *ptr){
-	int *f = (int *)ptr;
-  *f = 123;
-  	  
-  /*printf("add f-> %p\n", (void *) f);
-  printf("------------------\n");*/
-}
 
 int main(){
 	const int val = 10;
-	const int *mptr;
+	int *mptr;
 	mptr = &val;
 
+  *mptr = 123;
+  
   /*printf("add of val %p\n", (void *)&val);	  
   printf("add mptr-> %p\n", (void *) mptr);
   printf("add of ptstr = %p\n", (void *) &mptr); //The argument shall be a pointer to void 
   printf("------------------\n");*/
-
-	func(mptr);
 
   /*printf("int val = %d\n", val);*/
 

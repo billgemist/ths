a0b3fd8a43090d15e3ed41dce6581ca90ad6dee7
billgemist <billgemist@gmail.com>
2019-05-20 14:53:37 +0200

    latest

--- ubnum/ub_9/ub9_2_m.c
+++ ubnum/ub_9/ub9_2_m.c
@@ -2,25 +2,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int* func2(int *ptr){
-	int *f2;
-	*f2 = *ptr + 10;
-	return f2;
+int *gl_ptr;
+
+void func2(){
+	int a = 23;
+	gl_ptr = &a;
 }
 
-int* func1(){
-	int* f1ptr = func2(f1ptr);
-	return f1ptr;
+void func1(){
+	func2();
 }
 
 int main(){
-	int *ptr;
-
-	ptr = func1();
-	*ptr = *ptr + 1;
-
-	printf("ptr = %d\n", *ptr); //will be removed
-	
+	func1();
+	printf("gl_ptr = %d\n", *gl_ptr); //will be removed
 	return 0;
 }
 

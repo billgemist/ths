6cd26255a57e2a30ab5de21a02460786005352f6
billgemist <billgemist@gmail.com>
2019-05-22 11:44:23 +0200

    May 22nd(3rd)

--- ubnum/ub_55/ub55.c
+++ ubnum/ub_55/ub55.c
@@ -1,6 +1,8 @@
 //undefined behavior number 55
 #include <stdio.h>
-
+/*An address constant is a null pointer, 
+a pointer to an lvalue designating an object of static
+storage duration*/
 int main(){
 	
 	static int a = 10;
@@ -8,6 +10,8 @@ int main(){
 	int *ptr = &a;
 
 	int b = *ptr;
+
+	printf("b: %d\n", b);
     
 	return 0;
 }
